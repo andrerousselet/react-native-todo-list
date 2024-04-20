c6b1e5f73580573d4a54b62c2df72df3d7ae562f
Andre Rousselet <andre098@hotmail.com>
2024-04-20 15:46:35 -0300

    feat: add

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export function Home() {
-  return <Text>Hello world</Text>;
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-white text-lg">Hello world!!</Text>
+    </View>
+  );
 }
