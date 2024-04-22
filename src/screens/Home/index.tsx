d14de2ba81387622941ddeef4978fb3c68e36ca2
Andre Rousselet <andre098@hotmail.com>
2024-04-22 17:41:49 -0300

    feat: add function to handle adding new tasks

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,19 +1,61 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import {
+  Alert,
+  FlatList,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export function Home() {
+  const [tasks, setTasks] = useState<string[]>([]);
+  const [newTask, setNewTask] = useState("");
+
+  function handleAddTask() {
+    if (!newTask) {
+      return Alert.alert("Add a new task!");
+    }
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+    setNewTask("");
+  }
+
   return (
-    <View className="relative flex-1 items-center justify-center">
-      <View className="absolute -top-6 flex-row items-center justify-center px-2">
+    <View className="flex-1 items-center justify-center">
+      <View className="-top-6 flex-row items-center justify-center px-4 gap-x-2">
         <TextInput
           placeholder="Adicione uma nova tarefa"
           placeholderTextColor="#a1a1aa"
-          className="text-zinc-200 p-4 rounded-md bg-zinc-800 top-0 flex-1 h-12 mr-2"
+          className="text-zinc-200 p-4 rounded-md bg-zinc-800 top-0 flex-1 h-12"
+          value={newTask}
+          onChangeText={setNewTask}
         />
-        <TouchableOpacity className="rounded-md bg-sky-800 h-12 w-12 items-center justify-center">
-          <Text className="text-zinc-200 text-lg">+</Text>
+        <TouchableOpacity
+          className="relative rounded-md bg-sky-700 h-12 w-12 flex items-center justify-center"
+          onPress={handleAddTask}
+        >
+          <View className="bg-sky-600 rounded-full w-5 h-5 border border-zinc-200 flex items-center justify-center">
+            <Text className="text-zinc-200 text-center">+</Text>
+          </View>
         </TouchableOpacity>
       </View>
-      <Text className="text-zinc-100 text-lg">Hello world!!</Text>
+      <View className="flex-1 items-center justify-center">
+        <FlatList
+          data={tasks}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          ListEmptyComponent={() => (
+            <>
+              <Text className="text-zinc-400">
+                Você ainda não tem tarefas cadastradas.
+              </Text>
+              <Text className="text-zinc-500">
+                Crie tarefas e organize seus itens a fazer.
+              </Text>
+            </>
+          )}
+        />
+      </View>
     </View>
   );
 }
