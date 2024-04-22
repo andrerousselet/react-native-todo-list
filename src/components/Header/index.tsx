31b64b2c0a2cbacdb73fdefa185935f92663fe33
Andre Rousselet <andre098@hotmail.com>
2024-04-22 19:07:29 -0300

    refactor: add style to app logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export function Header() {
   return (
-    <View className="items-center justify-center w-full h-1/5 bg-zinc-950">
-      <Text className="text-zinc-100 text-xl">TODO</Text>
+    <View className="flex-row items-center justify-center w-screen h-1/5 bg-zinc-950">
+      <Text className="text-sky-400 text-3xl font-light">TO</Text>
+      <Text className="text-sky-700 text-3xl font-light">DO</Text>
+      <Ionicons name="checkmark-done" size={32} color="#38bdf8" />
     </View>
   );
 }
